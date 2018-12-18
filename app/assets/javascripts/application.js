fe1a1b942109125be0182721992104bc282e903c
rodroguett <rodroguett@gmail.com>
2018-12-18 01:51:54 -0300

    tabla con letra pequeña, grafico con enteros y filtro por status

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require Chart.bundle
 //= require chartkick
 //= require rails-ujs
 //= require activestorage
